b2f25f615f88def68afedd94cc156360b8488359
ssj5638 <ssj5638@gmail.com>
2018-06-26 14:46:58 +0900

    0625_웹 서버 라이브러리

--- ex2-5.py
+++ ex2-5.py
@@ -1,12 +1,12 @@
-# http.client.HTTPConnection 사용한 GET방식 요청
+# http.client.HTTPConnection 사용한 HEAD방식 요청
 
 from http.client import HTTPConnection
 
 conn = HTTPConnection('www.example.com')
-conn.request('GET', '/')
+conn.request('HEAD', '/')
 
 result = conn.getresponse()
 print(result.status, result.reason)
 
 data = result.read()
-print(data)
+print(len(data))
---
+++ myhttpserver1.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+port = '9999'
+
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):       # 서버가 GET방식으로 받은 경우 실행
+        # print('receive result')
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        self.wfile.write('<h1> Hello World</h1>'.encode('utf-8'))
+
+# 서버구동
+httpd = HTTPServer(('', 9999), MyHTTPRequestHandler)
+print('HTTP Server Starts....')
+httpd.serve_forever()
